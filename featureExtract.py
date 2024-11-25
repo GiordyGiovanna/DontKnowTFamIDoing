9d4d7111cad9b599709d89d855c90bd4f9dd2cff
Paolo Giordano <paolo.giordano494@edu.unito.it>
2024-11-25 21:49:45 +0100

    05 - Bro i swear, next thing i'm gonan do is rearrenge files
    (# yeah bro, im fucking scared)

--- featureExtract.py
+++ featureExtract.py
@@ -1,15 +1,21 @@
-import submission, util ## OKKKKKKKKKKK buddy, not strage.....
+import submission, util ## OKKKKKKKKKKK buddy, not strage at all.....
 from collections import defaultdict
 
+# ok, thats what I understood: we are creating a function that defins feature vector phi, not sure how it fucking works, on it
+
 #readExamples
-trainExp = util.readExamples("yeah.ImNotPushingThisFile") # I FUCKING DO, not pushing it 
-devExp = util.readExamples("yeah.ImNotPushingThisFile")
+trainExp = util.readExamples("names.train") # I FUCKING DO HAVE IT, not pushing it 
+devExp = util.readExamples("names.dev")
 
 def featureExtractor(x): #phi(x)
     phi = defaultdict(float)
     tokens = x.split()
-    left, entity, reght = tokens[0], tokens[1:-1], tokens[-1]
+    left, entity, right = tokens[0], tokens[1:-1], tokens[-1]
     phi['entity is ' + ' '.join(entity)] = 1
+    phi['entity left is ' + left] = 1
+    phi['entity right is ' + right] = 1
+    for word in entity:
+        phi['word is ' + word] = 1
     return phi
 # yeah bro, im fucking scared
 
